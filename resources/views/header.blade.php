cdab67e7f55007acf6337b3e5cb8b90fcf30f97f
Elikill58 <arpetzouille@gmail.com>
2022-08-13 10:28:28 +0200

    Add search input on navbar

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -2,7 +2,7 @@
     <link href="{{ plugin_asset('positivity', 'css/style.css') }} " rel="stylesheet">
 @endpush
 
-<div class="py-3">
+<div class="py-3 banner">
     <?php
     $features = array("accounts", "verifications");
     if(setting("positivity.has_bans")) {
@@ -18,4 +18,20 @@
             </a>
         @endcan
     @endforeach
+    <div class="banner-search">
+        <input type="text" class="form-control" id="playername" name="playername" required>
+        <a type="submit" value="submit request" class="btn btn-primary" onclick="return checkValidation(document.getElementById('playername').value)">
+            {{ trans('messages.actions.search') }}
+            <span role="status"></span>
+        </a>
+    </div>
 </div>
+
+@push('scripts')
+    <script>
+        function checkValidation(name) {
+            window.location.href = "{{ route('positivity.accounts.show', ['account' => 'UUID']) }}".replace('UUID', name);
+            return false;
+        }
+    </script>
+@endpush
